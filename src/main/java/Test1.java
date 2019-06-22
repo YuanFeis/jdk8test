fc8b46e261fd8923ace2c3c2434e5b35c38bdf40
yuanfei <15531457597@163.com>
2019-06-22 14:08:21 +0800

    Initial commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,14 @@
+public class Test1 {
+    public static void main(String[] args) {
+        Theinterface1 ti1= () -> {};
+        System.out.println(ti1);
+        Theinterface2 ti2 =() ->{};
+        System.out.println(ti2);
+    }
+}
+interface  Theinterface1{
+    void mymethod1();
+}
+interface  Theinterface2{
+    void mymethod2();
+}
